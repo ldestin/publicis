f0f03f671c878eebee5f923d869f4055b98cd35a
ldestin <loick.destin@gmail.com>
2020-12-10 15:41:08 +0100

    cors

---
+++ pages/api/cors.js
@@ -0,0 +1,34 @@
+import Cors from 'cors'
+
+const cors = Cors({
+  // Only allow requests with GET, POST and OPTIONS
+  methods: ['GET', 'POST', 'OPTIONS'],
+})
+
+// Initialize the cors middleware
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+  // Run cors
+  await runMiddleware(req, res, cors)
+
+  console.log({ res })
+  res.writeHead(302, {
+    'Location': '/'
+    //add other headers here...
+  })
+  // Rest of the API logic
+  res.end()
+}

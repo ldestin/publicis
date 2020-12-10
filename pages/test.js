794b676d202a2d38fbbb9bfb6ba235bfed372e15
ldestin <loick.destin@gmail.com>
2020-12-10 16:08:42 +0100

    test in test page

--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import Cors from 'cors'
+
+const cors = Cors({
+  // Only allow requests with GET, POST and OPTIONS
+  methods: ['GET', 'POST', 'OPTIONS'],
+})
+
+export const getServerSideProps = async ({ req, res }) => {
+  await cors(req, res, () => {})
+
+  return { props: {} }
+}
 
 export default () => (
   <div>
